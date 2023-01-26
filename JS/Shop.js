88ad96091fce1d8cd67ed0411b264512f6b942be
Arteom23 <118016842+Arteom23@users.noreply.github.com>
2023-01-26 11:08:16 -0800

    Add files via upload

---
+++ JS/Shop.js
@@ -0,0 +1,14 @@
+$(function(){
+    // Mixitup
+    var containerEl = document.querySelector('.mixitup');
+
+    var mixer = mixitup(containerEl);
+    // Mixitup
+
+    // Hide
+    $(".Load").click(function(){
+        $(".hideShow").slideToggle(1000);
+    })
+    
+    // Hide
+})
